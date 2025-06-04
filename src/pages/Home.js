7b17f638b4cae0e9d13b6e07b90c19ce19b229fb
brisatraverso <traversobrisa1@gmail.com>
2025-06-04 11:38:54 -0300

    actualizacion1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,16 @@ const Home = async () => {
   const view = `
     <div class="Rockets">
       ${
-        rockets?.results
-          ? rockets.results.map(rocket => `
+        Array.isArray(rockets) && rockets.length
+          ? rockets.map(rocket => `
             <article class="Rocket-item">
               <a href="#/${rocket.id}/">
-                <img src="${rocket.links.patch.small}" alt="${rocket.name}">
+                <img src="${rocket.links?.patch?.small || 'placeholder.png'}" alt="${rocket.name}">
                 <h2>${rocket.name}</h2>
               </a>
             </article>
           `).join('')
-          : '<p>No hay cohetes disponibles.</p>'
+        : '<p>No hay cohetes disponibles.</p>'
       }
     </div>
   `;
