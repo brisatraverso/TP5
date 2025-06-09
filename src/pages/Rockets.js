2c7afe8c1f23da1d1872184c7cc1e10b0524e77c
brisatraverso <traversobrisa1@gmail.com>
2025-06-09 10:04:28 -0300

    prueba

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,26 +1,22 @@
 import getHash from '../utils/getHash';
 import getData from '../utils/getData';
 
-const Character =  async () => {
+const Rocket =  async () => {
 
     const id = getHash();
     const rocket = await getData(id);
     const view = `
         <div class="Rockets-inner">
             <article class="Rocket-card">
-                <img src="${rocket.image}" alt="${rocket.name}">
                 <h2>${rocket.name}</h2>
-            </article>
-
-            <article class="Rockets-card">
-                <h3>Fallas: <span>${rocket.failures}</span></h3>
-                <h3>Imagen:<span>${rocket.links.patch.small}</span></ </h3>
-                <h3>Campo de detalle: <span>${rocket.details}</span></</h3>
+                <img src="${rocket.links.patch.small || ''}" alt="${rocket.name}">
+                <h3>Fallas: <span>${rocket.failures.length > 0 ? rocket.failures.map(f =>f.reason).join(','):'Ninguna'}</span></h3>
+                <h3>Campo de detalle: <span>${rocket.details || 'No hay detalles'}</span></</h3>
                 <h3>Número de vuelo: <span>${rocket.flight_number}</span></</h3>
-                <h3>Fecha y hora de despegue (UTC): <span>${rocket.date_utc}</span></</h3>
+                <h3>Fecha y hora de despegue (UTC): <span>${new Date(rocket.date_utc).toLocaleString()}</span></</h3>
             </article>
         </div> 
     `
     return view
 };
-export default Character;
+export default Rocket;
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,7 @@
 const API = 'https://api.spacexdata.com/v5/launches';
 
 const getData = async (id) => {
-    const apiURL = id ? `${API}${id}` : API;
+    const apiURL = id ? `${API}/${id}` : API;
 
     try {
         const response = await fetch(apiURL);
